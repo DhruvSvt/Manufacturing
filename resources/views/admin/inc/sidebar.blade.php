97013148fca73a851422cd9746c75d72a193f00b
DhruvSvt <dhruvsvt08@gmail.com>
2023-10-05 11:16:58 +0530

    Update sidebar.blade.php

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -333,18 +333,18 @@
                                         Create
                                     </a>
                                 </li>
-                                {{-- <li>
+                                <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
                                         href="#" :class="page === 'gifts' && '!text-white'">
-                                        Items
+                                        DAILY PRODUCTION REPORT
                                     </a>
                                 </li>
                                 <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
                                         href="#" :class="page === 'products' && '!text-white'">
-                                        Products
+                                        LAB TESTING REPORT
                                     </a>
-                                </li> --}}
+                                </li>
                             </ul>
                         </div>
                         <!-- Dropdown Menu End -->
