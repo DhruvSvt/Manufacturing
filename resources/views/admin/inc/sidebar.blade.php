c4e5d1b2212066127db11b69446812403f8480b5
Dhruv7987 <dhruvsvt08@gmail.com>
2023-10-11 21:00:44 +0530

    some minor changes

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -152,7 +152,8 @@
                                 </li>
                                 <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
-                                        href="{{ route('brand') }}" :class="page === 'settings' && '!text-white'" data-modal-target="authentication-modal"
+                                        href="{{ route('brand') }}" :class="page === 'settings' && '!text-white'"
+                                        data-modal-target="authentication-modal"
                                         data-modal-toggle="authentication-modal">
                                         Brand
                                     </a>
@@ -385,7 +386,8 @@
                                 </li>
                                 <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
-                                        href="{{ route('product-detail') }}" :class="page === 'products' && '!text-white'">
+                                        href="{{ route('product-detail') }}"
+                                        :class="page === 'products' && '!text-white'">
                                         Finnish Products
                                     </a>
                                 </li>
@@ -428,13 +430,13 @@
                                 <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
                                         href="#" :class="page === 'gifts' && '!text-white'">
-                                        DAILY PRODUCTION REPORT
+                                        Daily Production Report
                                     </a>
                                 </li>
                                 <li>
                                     <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
                                         href="#" :class="page === 'products' && '!text-white'">
-                                        LAB TESTING REPORT
+                                        Lab Testing Report
                                     </a>
                                 </li>
                             </ul>
@@ -442,6 +444,54 @@
                         <!-- Dropdown Menu End -->
                     </li>
                     <!-- Menu Item Production -->
+
+                    <!-- Menu Item Challans -->
+                    <li>
+                        <a class="group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                            href="#" @click.prevent="selected = (selected === 'Challans' ? '':'Challans')"
+                            :class="{
+                                'bg-graydark dark:bg-meta-4': (selected === 'Challans') || (page === 'raw-material' ||
+                                    page === 'gifts' || page === 'products')
+                            }">
+                            <i class="fa-solid fa-receipt"></i>
+                            Challans
+                            <svg class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
+                                :class="{ 'rotate-180': (selected === 'Task') }" width="20" height="20"
+                                viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                                    fill="" />
+                            </svg>
+                        </a>
+
+                        <!-- Dropdown Menu Start -->
+                        <div class="translate transform overflow-hidden"
+                            :class="(selected === 'Challans') ? 'block' : 'hidden'">
+                            <ul class="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
+                                <li>
+                                    <a class="group relative flex items--center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                        href="" :class="page === 'raw-material' && '!text-white'">
+                                        Raw Material Issue Challan
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                        href="#" :class="page === 'products' && '!text-white'">
+                                        Finish Goods Challan
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                        href="#" :class="page === 'gifts' && '!text-white'">
+                                        Gift Issue Challan
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Dropdown Menu End -->
+                    </li>
+                    <!-- Menu Item Challans -->
+
 
                     <!-- Menu Item Report -->
                     <li>
@@ -490,7 +540,7 @@
                         </div>
                         <!-- Dropdown Menu End -->
                     </li>
-                    <!-- Menu Item Stocks -->
+                    <!-- Menu Item Report -->
 
                 </ul>
             </div>
