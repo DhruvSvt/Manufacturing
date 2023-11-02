00b646924d164d3db14834767b4104b7f0097b45
DhruvSvt <dhruvsvt08@gmail.com>
2023-11-02 17:58:25 +0530

    Update create.blade.php

--- resources/views/admin/production/create.blade.php
+++ resources/views/admin/production/create.blade.php
@@ -35,7 +35,7 @@
                 </div>
                 <div class="w-full xl:w-1/2">
                     <label class="mb-2.5 block text-black dark:text-white">
-                        Quantity <span class="text-meta-1">*</span>
+                        Batch Size <span class="text-meta-1">*</span>
                     </label>
                     <input type="number" placeholder="Enter the Quantity" name="qty"
                         class="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
