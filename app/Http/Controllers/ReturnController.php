e93165952d11d8f076fcdbc353bfbcf0a41d2e87
DhruvSvt <dhruvsvt08@gmail.com>
2023-11-08 18:05:19 +0530

    Update stock functionality

--- app/Http/Controllers/ReturnController.php
+++ app/Http/Controllers/ReturnController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Production;
+use App\Models\ProductStock;
+use App\Models\ReturnGood;
 use App\Models\Suppliers;
 use Illuminate\Http\Request;
 
@@ -10,13 +13,58 @@ class ReturnController extends Controller
 {
     public function good_return()
     {
-        return view('admin.returns.good-return');
+        $returns = ReturnGood::all();
+        return view('admin.returns.good-return', compact('returns'));
     }
 
     public function good_return_create()
     {
         $suppilers = Suppliers::all();
         $products = Product::all();
-        return view('admin.returns.good-return-create',compact('suppilers','products'));
+        return view('admin.returns.good-return-create', compact('suppilers', 'products'));
+    }
+
+    public function good_return_store(Request $request)
+    {
+
+        $request->validate([
+            'product_id' => 'required',
+            'supplier_id' => 'required',
+            'type' => 'required',
+            'batch' => 'required',
+            'builty' => 'required',
+            'transport' => 'required',
+            'dispatch' => 'required',
+            'date_of_receipt' => 'required',
+            'quantity' => 'required',
+            'receipt' => 'required',
+        ]);
+
+        $stock = Production::where('batch_no',$request->batch)->first();
+
+        if($request->product_id == $stock->product_id )
+        {
+            $expiry_date = $stock->expiry_date;
+            $current_date = \Carbon\Carbon::now();
+
+            if($expiry_date > $current_date){
+                // Update in Production
+                $stock->qty += $request->quantity;
+                $stock->save();
+
+                // Update in ProductionStock
+                $product_stock = ProductStock::where('purchase_id',$stock->id)->first();
+                $product_stock->quantity += $request->quantity;
+                $product_stock->save();
+            }
+
+            ReturnGood::create($request->post());
+
+            return redirect()->route('good-return')->with('success', 'Return Good Create Successfully.');
+        }
+        else{
+            return redirect()->back()->with('error', 'Your Product and Batch No. not matches with our records !!');
+        }
+
     }
 }
