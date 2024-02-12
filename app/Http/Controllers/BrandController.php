5b5d416c0c1608f44e5c8960e7bb9334dcc279b9
DhruvSvt <dhruvsvt08@gmail.com>
2024-02-12 13:29:58 +0530

    brand search and pagination

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class BrandController extends Controller
 {
     public function index()
     {
-        $brands = Brand::all();
-        return view('admin.brand',compact('brands'));
+        /* Query Parameters */
+        $keyword = request()->keyword;
+        $rows = request()->rows ?? 25;
+
+        if ($rows == 'all') {
+            $rows = Brand::count();
+        }
+
+        // Get the table columns
+        $allColumns = Schema::getColumnListing((new Brand())->getTable());
+
+        $brands = Brand::when(isset($keyword), function ($query) use ($keyword, $allColumns) {
+                $query->where(function ($query) use ($keyword, $allColumns) {
+                    // Dynamically construct the search query
+                    foreach ($allColumns as $column) {
+                        $query->orWhere($column, 'LIKE', "%$keyword%");
+                    }
+                });
+            })
+            ->latest()
+            ->paginate($rows);
+
+        return view('admin.brand', compact('brands'));
     }
 
     public function store(Request $request)
@@ -21,7 +43,7 @@ class BrandController extends Controller
 
         $brand = new Brand;
         $brand->name = $request->name;
-        $brand->save()  ;
+        $brand->save();
 
         return redirect()->route('brand');
     }
