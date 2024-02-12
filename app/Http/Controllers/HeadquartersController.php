cb03cfd33916037a79865b5eb06c6ef899f9b814
DhruvSvt <dhruvsvt08@gmail.com>
2024-02-12 13:21:26 +0530

    headquarter search and pagination

--- app/Http/Controllers/HeadquartersController.php
+++ app/Http/Controllers/HeadquartersController.php
@@ -4,12 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\Headquarters;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class HeadquartersController extends Controller
 {
     public function index()
     {
-        $headquarters = Headquarters::all();
+        /* Query Parameters */
+        $keyword = request()->keyword;
+        $rows = request()->rows ?? 25;
+
+        if ($rows == 'all') {
+            $rows = Headquarters::count();
+        }
+
+        // Get the table columns
+        $allColumns = Schema::getColumnListing((new Headquarters())->getTable());
+
+        $headquarters = Headquarters::when(isset($keyword), function ($query) use ($keyword, $allColumns) {
+                $query->where(function ($query) use ($keyword, $allColumns) {
+                    // Dynamically construct the search query
+                    foreach ($allColumns as $column) {
+                        $query->orWhere($column, 'LIKE', "%$keyword%");
+                    }
+                });
+            })
+            ->latest()
+            ->paginate($rows);
+
         return view('admin.headquarters', compact('headquarters'));
     }
 
