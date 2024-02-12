ec6efa0b76d3e398ae3caf729c0ae194695d5d99
DhruvSvt <dhruvsvt08@gmail.com>
2024-02-12 12:48:33 +0530

    search & pagination in raw-material

--- app/Http/Controllers/RawMaterialController.php
+++ app/Http/Controllers/RawMaterialController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\RawMaterial;
 use App\Models\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class RawMaterialController extends Controller
 {
@@ -15,7 +16,31 @@ class RawMaterialController extends Controller
      */
     public function index()
     {
-        $raw_material = RawMaterial::latest()->get();
+        /* Query Parameters */
+        $keyword = request()->keyword;
+        $rows = request()->rows ?? 25;
+
+        if ($rows == 'all') {
+            $rows = RawMaterial::count();
+        }
+
+        // Get the table columns
+        $allColumns = Schema::getColumnListing((new RawMaterial())->getTable());
+
+        // $raw_material = RawMaterial::latest()->get();
+
+        $raw_material = RawMaterial::with('parent')
+            ->when(isset($keyword), function ($query) use ($keyword, $allColumns) {
+                $query->where(function ($query) use ($keyword, $allColumns) {
+                    // Dynamically construct the search query
+                    foreach ($allColumns as $column) {
+                        $query->orWhere($column, 'LIKE', "%$keyword%");
+                    }
+                });
+            })
+            ->latest()
+            ->paginate($rows);
+
         return view('admin.raw-material', compact('raw_material'));
     }
 
@@ -74,7 +99,7 @@ class RawMaterialController extends Controller
     {
         $units = Unit::whereStatus(true)->get();
         $raw_material = RawMaterial::find($id);
-        return view('admin.raw-material-edit', compact('raw_material','units'));
+        return view('admin.raw-material-edit', compact('raw_material', 'units'));
     }
 
     /**
@@ -117,8 +142,8 @@ class RawMaterialController extends Controller
     {
         $raw_material = RawMaterial::findOrFail($request->raw_material_id);
         $raw_material->status = $request->status;
-        $raw_material->save();  
+        $raw_material->save();
 
-        return redirect()->back();  
+        return redirect()->back();
     }
 }
