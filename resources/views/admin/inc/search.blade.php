69df320d55e624e60c1b10752a40ecbfca37967c
DhruvSvt <dhruvsvt08@gmail.com>
2024-02-16 11:56:48 +0530

    Update search.blade.php

--- resources/views/admin/inc/search.blade.php
+++ resources/views/admin/inc/search.blade.php
@@ -27,7 +27,7 @@ $row = $_GET['rows'] ?? '25';
             <div class="relative">
                 <input type="search" id="default-search" name="keyword" value="{{ $_GET['keyword'] ?? '' }}"
                     class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search Mockups, Logos...">
+                    placeholder="Enter relevant keyword..">
                 <button type="submit"
                     class="text-white absolute end-2 bottom-auto top-[4px] bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
