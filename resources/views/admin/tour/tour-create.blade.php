e6ba0ccf8cd3a2f43a4c6e7feac4b2ec1f732d46
DhruvSvt <dhruvsvt08@gmail.com>
2024-03-22 16:24:42 +0530

    Update tour-create.blade.php

--- resources/views/admin/tour/tour-create.blade.php
+++ resources/views/admin/tour/tour-create.blade.php
@@ -141,8 +141,9 @@
                                                 </a>
                                                 <button type="button"
                                                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gifts</button>
-                                                <button type="button"
+                                                {{-- <button type="button"
                                                     class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Visiting</button>
+                                                --}}
                                             </div>
                                         </td>
                                     </tr>
